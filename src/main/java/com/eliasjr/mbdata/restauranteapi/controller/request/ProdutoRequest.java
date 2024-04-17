a06144ab362a53bf4d5b6fe8ece1020cbb0518c6
EliasJnnr <elias.dvlpr@gmail.com>
2024-04-17 20:11:49 -0300

    feat handler exception dto request

--- src/main/java/com/eliasjr/mbdata/restauranteapi/controller/request/ProdutoRequest.java
+++ src/main/java/com/eliasjr/mbdata/restauranteapi/controller/request/ProdutoRequest.java
@@ -1,13 +1,20 @@
 package com.eliasjr.mbdata.restauranteapi.controller.request;
 
 import com.eliasjr.mbdata.restauranteapi.model.enums.Categoria;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
 @Getter
 public class ProdutoRequest {
+
+    @NotNull
+    @NotBlank
     private String nome;
+    @NotNull
     private BigDecimal preco;
+    @NotNull
     private Categoria categoria;
 }
