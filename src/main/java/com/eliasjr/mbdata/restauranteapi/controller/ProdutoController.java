943eb7da52a19f776c4a2d082491d88db927942b
EliasJnnr <elias.dvlpr@gmail.com>
2024-04-17 21:10:32 -0300

    default values pageable

--- src/main/java/com/eliasjr/mbdata/restauranteapi/controller/ProdutoController.java
+++ src/main/java/com/eliasjr/mbdata/restauranteapi/controller/ProdutoController.java
@@ -9,6 +9,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +30,7 @@ public class ProdutoController {
 
     @GetMapping("/list")
     @ApiResponse(description = "Lista todos os produtos paginados")
-    public ResponseEntity<Page<Produto>> list(Pageable pageable) {
+    public ResponseEntity<Page<Produto>> list(@PageableDefault(size = 10, sort = "nome", direction = Sort.Direction.ASC) Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK).body(produtoService.listarProdutos(pageable));
     }
 
